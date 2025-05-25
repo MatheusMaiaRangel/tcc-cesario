204bfacb8e0309c8cda5c0d75b55270b27be44de
MatheusMaiaRangel <matheusmaiarangel@gmail.com>
2025-05-25 20:19:01 -0300

    Navbar de admin

--- calendario_admin.php
+++ calendario_admin.php
@@ -28,29 +28,16 @@ if (!isset($_SESSION['usuario'])) {
     <body class="main">
 
       <!--navbar-->
-      <nav class="navbar fixed-top background_blue">
+      <nav class="navbar fixed-top navbar-expand-lg background_blue">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Nome da escola</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <a class="navbar-brand text-white" href="index.php">Oblivion</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">icone da escola</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Cardápio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link blue" href="#">Calendário</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Comunicados</a>
-                  </li>
-              </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                  <li class="nav-item"><a class="nav-link text-white" href="gerenciar_materias.php">Matérias</a></li>
+                </ul>
             </div>
           </div>
         </nav>
